40e6b3b83504234af787bf3aee643c1bf7d889b0
jasonwong7770 <jasonwong7770@gmail.com>
2026-05-31 04:00:59 -0400

    Front end interface done

--- Login Page/src/pages/Login.tsx
+++ Login Page/src/pages/Login.tsx
@@ -1,11 +1,24 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 function Login(){
+  const [identifier, setIdentifier] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+
+  function handleLogin() {
+    console.log(identifier, password)
+  }
+
   return (
     <div>
       <nav>
         <Link to="/">Home</Link>
       </nav>
+      <input type="text" placeholder="Username or Email" value={identifier} onChange={(e) => setIdentifier(e.target.value)} /><br/>
+      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} /><br/>
+      <button onClick={handleLogin}>Login</button> <br/>
+      {error && <p className="text-red-500">{error}</p>}
     </div>
   )
 }
--- Login Page/src/pages/Verify.tsx
+++ Login Page/src/pages/Verify.tsx
@@ -7,6 +7,10 @@ function Verify() {
   const location = useLocation()
   const navigate = useNavigate()
 
+  function handleVerify() {
+    console.log(otp)
+  }
+
   useEffect(() => {
     if (!location.state) {
       navigate('/')
@@ -21,6 +25,8 @@ function Verify() {
     <>
       <h1>Welcome {username}!</h1>
       <p>A code has been sent to {email}</p>
+      <input type="text" placeholder="Enter 6-digit code" value={otp} onChange={(e) => setOtp(e.target.value)} maxLength={6}/><br/>
+      <button onClick={handleVerify}>Verify</button>
     </>
   )
 }
